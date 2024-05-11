e08a70333c1aa554510da4f8d2d290458cb10c71
Lucas <git@sl.thesqooid.com>
2024-05-11 22:57:24 +1000

    implement gene bfs

--- src/compute_functions/utils.rs
+++ src/compute_functions/utils.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, vec};
 
 use super::ComputeFunction;
 
@@ -8,15 +8,48 @@ impl ComputeFunction {
     pub fn bfs<'a>(&'a self) -> Vec<(&'a Self, &'a Self)> {
         let mut frontier: VecDeque<(&ComputeFunction, &ComputeFunction)> =
             VecDeque::from([(self, self)]);
+        let mut nodes: Vec<(&ComputeFunction, &ComputeFunction)> = vec![];
         while !frontier.is_empty() {
             // Can unwrap because we already checked that it's not empty
             let (current, parent) = frontier.pop_front().unwrap();
+            nodes.push((current, parent));
             match current {
-                ComputeFunction::Constant(inner) => {}
+                ComputeFunction::Constant(_inner) => {}
                 ComputeFunction::One(inner) => frontier.push_back((inner.arg(), current)),
-                ComputeFunction::Two(inner) => frontier.push_back((inner.arg(0), current)),
+                ComputeFunction::Two(inner) => {
+                    frontier.push_back((inner.arg(0usize), current));
+                    frontier.push_back((inner.arg(1usize), current))
+                }
             }
         }
-        todo!()
+        return nodes;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::compute_functions::{ConstantFunction, SingleArgFunction, TwoArgFunction};
+
+    use super::*;
+
+    #[test]
+    fn test_bfs() {
+        // Add
+        //     Sin
+        //         Coord(0)
+        //     Loge
+        //         Constant(0.1,0.2,0.3)
+        let compute_function = ComputeFunction::Two(Box::new(TwoArgFunction::Add(
+            ComputeFunction::One(Box::new(SingleArgFunction::Sin(ComputeFunction::Constant(
+                Box::new(ConstantFunction::Coord(0)),
+            )))),
+            ComputeFunction::One(Box::new(SingleArgFunction::Loge(
+                ComputeFunction::Constant(Box::new(ConstantFunction::Constant(0.1, 0.2, 0.3))),
+            ))),
+        )));
+        let nodes = compute_function.bfs();
+        for node in &nodes {
+            println!("{:?}", node);
+        }
     }
 }
